59a4fdfdb2522492e5c373ae07c3795b7c6abadf
Qinhu Wang <qinhu.wang@icloud.com>
2017-10-20 07:14:05 +0800

    fit umi_tools

--- 012.filter_fastq.pl
+++ 012.filter_fastq.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 #===============================================================================
 #
-# Description: Filter low quanlity fastq
+# Description: Filter poly-T and short reads, format fastq for umi_tools
 #
 # Copyright (c) 2017 Northwest A&F University
 # Author: Qinhu Wang
@@ -15,8 +15,7 @@ use Data::Dumper;
 
 my $fastq_input = $ARGV[0] || "input.fq";
 my $fastq_output = $ARGV[1] || "output.fq";
-my $qual_offset = 33;
-my $qual_threshold = 27;
+my $qual_threshold = 20;
 my $poly_T_len = 10;
 my $seq_len_thresh = 30;
 
@@ -24,11 +23,15 @@ filter_fastq();
 
 sub filter_fastq {
 	open (FQI, $fastq_input) or die "Cannot open fastq: $fastq_input, $!\n";
+
+	my @n = split /\//, $fastq_input;
+	my $tiss = $n[-2];
+	my $cell = $n[-1];
+	$cell =~ s/\.fq//;
+
 	my %fastq;
 	my ($fq_1, $fq_2, $fq_3, $fq_4, $rmt, $rmt_qual);
-	my @rmt_qual;
-	my $rmt_flag = 0;
-	my ($read_count, $low_rmt_count, $short_read_count, $pcr_dup_count, $clean_count);
+	my ($read_count, $short_read_count, $clean_count);
 	while ($fq_1 = <FQI>) {
 		$fq_2 = <FQI>;
 		$fq_3 = <FQI>;
@@ -36,80 +39,34 @@ sub filter_fastq {
 		chomp $fq_2;
 		chomp $fq_4;
 		$read_count++;
-		# reset vars
-		@rmt_qual = ();
-		$rmt_flag = 0;
-		# filter low qual RMT
+		# umi/rmt
 		($rmt, $rmt_qual) = split /\s/, $fq_1;
-		@rmt_qual = split //, $rmt_qual;
-		foreach my $q (@rmt_qual) {
-			my $num = (ord $q) - $qual_offset;
-			if ($num < $qual_threshold) {
-				$rmt_flag = 1;
-			}
-		}
-		if ($rmt_flag == 1) {
-			$low_rmt_count++;
-			next;
-		}
-		# filter polyT
+		$rmt =~ s/^\@//;
+		$rmt = "$tiss\_$cell\_$rmt";
+		# find poly T
 		if ($fq_2 =~ /(T{$poly_T_len})/) {
 			$fq_2 = $`;
+			# filter short reads
 			if (length $fq_2 < $seq_len_thresh) {
 				$short_read_count++;
 				next;
 			}
 			$fq_4 = substr($fq_4, 0, length($fq_2));
 		}
-		# filter PCR duplication
-		if (exists $fastq{$rmt}{$fq_2}) {
-			$pcr_dup_count++;
-			my ($low1, $sum1) = qual_info($fq_4);
-			my ($low2, $sum2) = qual_info($fastq{$rmt}{$fq_2});
-			if ($low1 > $low2) {
-				$fastq{$rmt}{$fq_2} = $fq_4;
-			} else {
-				if ($low1 == $low2 && $sum1 > $sum2) {
-					$fastq{$rmt}{$fq_2} = $fq_4;
-				}
-			}
-		} else {
-			$clean_count++;
-			$fastq{$rmt}{$fq_2} = $fq_4;
-		}
+		$clean_count++;
+		$fastq{$rmt}{$fq_2} = $fq_4;
 	}
 	close FQI;
 
-	my @n = split /\//, $fastq_input;
-	my $cell = $n[-2] . "\t" . $n[-1];
-	$cell =~ s/\.fq//;
-	print "$cell\t$read_count\t$clean_count\t$low_rmt_count\t$short_read_count\t$pcr_dup_count\n";
+	print "$cell\t$tiss\t$read_count\t$clean_count\n$short_read_count\n";
 
 	# write fastq
 	open (FQO, ">$fastq_output") or die "Cannot open fastq: $fastq_output, $!\n";
 	foreach my $rmt (sort keys %fastq) {
 		foreach my $read (sort keys $fastq{$rmt}) {
-			print FQO "$rmt\n$read\n+\n", $fastq{$rmt}{$read}, "\n";
+			print FQO "@" . "$rmt\n$read\n+\n", $fastq{$rmt}{$read}, "\n";
 		}
 	}
 	close FQO;
 }
 
-# quality information
-# input: quality string
-# output: lowest, sum of quality
-sub qual_info {
-	my $qual = shift;
-	my @w = split //, $qual;
-	my $sum = 0;
-	my $low = 100;
-	my $num = 0;
-	for (my $i = 0; $i < @w; $i++) {
-		$num = (ord $w[$i]) - $qual_offset;
-		$sum += $num;
-		if ($num < $low) {
-			$low = $num;
-		}
-	}
-	return ($low, $sum);
-}
